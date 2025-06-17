38cc84b45a3711fcdbc75d46e4c2555705af6aaf
Josh Brunton <josh.brunton@proton.me>
2025-06-17 15:51:43 +0100

    enum picker component

--- src/FlossApp.Application/Enums/ColorSchema.cs
+++ src/FlossApp.Application/Enums/ColorSchema.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ namespace FlossApp.Application.Enums;
 
 public enum ColorSchema
 {
+    [Display(Name="RGB")]
     Rgb,
+
+    [Display(Name= "DMC (Dollfus-Mieg et Compagnie)")]
     Dmc,
+
+    [Display(Name = "HTML (HyperText Markup Language)")]
     Html,
+
+    [Display(Name = "COPIC (\u30b3\u30d4\u30c3\u30af)")]
     Copic
 }
---
+++ src/FlossApp.Wasm/Extensions/System/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace FlossApp.Wasm.Extensions.System;
+
+public static class EnumExtensions
+{
+    public static string ToDisplayName(this Enum source)
+    {
+        string sourceString = source.ToString();
+        return source.GetType()
+            .GetMember(sourceString)
+            .FirstOrDefault()?
+            .GetCustomAttribute<DisplayAttribute>()?
+            .GetName() ?? sourceString;
+    }
+}
