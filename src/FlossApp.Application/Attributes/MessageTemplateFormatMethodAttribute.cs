024ec884cb6c256ec5f082496b4f7aa05717ccbc
Josh Brunton <josh.brunton@proton.me>
2025-06-19 10:48:01 +0100

    stuff

---
+++ src/FlossApp.Application/Attributes/MessageTemplateFormatMethodAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlossApp.Application.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class MessageTemplateFormatMethodAttribute : Attribute
+{
+    public string FormatParameterName { get; }
+
+    public MessageTemplateFormatMethodAttribute(string formatParameterName)
+    {
+        FormatParameterName = formatParameterName;
+    }
+}
