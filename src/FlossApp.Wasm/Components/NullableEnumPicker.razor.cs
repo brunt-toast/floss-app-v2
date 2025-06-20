0f34f8d75f81f6e2a2f0c823211b3c308ee23ab4
Josh Brunton <josh.brunton@proton.me>
2025-06-20 10:19:46 +0100

    Add nullable enum picker

---
+++ src/FlossApp.Wasm/Components/NullableEnumPicker.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FlossApp.Wasm.Components;
+
+public partial class NullableEnumPicker<T> where T : struct, Enum
+{
+    [Parameter]
+    public T? Value
+    {
+        get;
+        set
+        {
+            if (EqualityComparer<T?>.Default.Equals(field, value))
+            {
+                return;
+            }
+            field = value;
+            ValueChanged.InvokeAsync(value);
+        }
+    }
+
+    [Parameter]
+    public EventCallback<T?> ValueChanged { get; set; }
+
+    [Parameter]
+    public string Label
+    {
+        get;
+        set
+        {
+            if (EqualityComparer<string>.Default.Equals(field, value))
+            {
+                return;
+            }
+
+            field = value;
+            LabelChanged.InvokeAsync(value);
+        }
+    } = "";
+
+    [Parameter]
+    public EventCallback<string> LabelChanged { get; set; }
+}
