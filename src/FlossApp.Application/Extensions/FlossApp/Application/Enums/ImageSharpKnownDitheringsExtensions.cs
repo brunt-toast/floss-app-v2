ab3377387ac257b1c8345230c200ee55215b36e4
Josh Brunton <josh.brunton@proton.me>
2025-06-20 10:00:21 +0100

    Add converters

---
+++ src/FlossApp.Application/Extensions/FlossApp/Application/Enums/ImageSharpKnownDitheringsExtensions.cs
@@ -0,0 +1,31 @@
+﻿using FlossApp.Application.Enums;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Dithering;
+
+namespace FlossApp.Application.Extensions.FlossApp.Application.Enums;
+
+internal static class ImageSharpKnownDitheringsExtensions
+{
+    public static IDither? AsKnownDithering(this ImageSharpKnownDitherings? dither)
+    {
+        return dither switch
+        {
+            ImageSharpKnownDitherings.Bayer2x2 => KnownDitherings.Bayer2x2,
+            ImageSharpKnownDitherings.Ordered3x3 => KnownDitherings.Ordered3x3,
+            ImageSharpKnownDitherings.Bayer4x4 => KnownDitherings.Bayer4x4,
+            ImageSharpKnownDitherings.Bayer8x8 => KnownDitherings.Bayer8x8,
+            ImageSharpKnownDitherings.Bayer16x16 => KnownDitherings.Bayer16x16,
+            ImageSharpKnownDitherings.Atkinson => KnownDitherings.Atkinson,
+            ImageSharpKnownDitherings.Burks => KnownDitherings.Burks,
+            ImageSharpKnownDitherings.FloydSteinberg => KnownDitherings.FloydSteinberg,
+            ImageSharpKnownDitherings.JarvisJudiceNinke => KnownDitherings.JarvisJudiceNinke,
+            ImageSharpKnownDitherings.Sierra2 => KnownDitherings.Sierra2,
+            ImageSharpKnownDitherings.Sierra3 => KnownDitherings.Sierra3,
+            ImageSharpKnownDitherings.SierraLite => KnownDitherings.SierraLite,
+            ImageSharpKnownDitherings.StevensonArce => KnownDitherings.StevensonArce,
+            ImageSharpKnownDitherings.Stucki => KnownDitherings.Stucki,
+            null => null,
+            _ => throw new ArgumentOutOfRangeException(nameof(dither), dither, null)
+        };
+    }
+}
---
+++ src/FlossApp.Application/Extensions/FlossApp/Application/Enums/ImageSharpKnownResamplersExtensions.cs
@@ -0,0 +1,31 @@
+﻿using FlossApp.Application.Enums;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
+
+namespace FlossApp.Application.Extensions.FlossApp.Application.Enums;
+
+internal static class ImageSharpKnownResamplersExtensions
+{
+    public static IResampler AsKnownResampler(this ImageSharpKnownResamplers resampler)
+    {
+        return resampler switch
+        {
+            ImageSharpKnownResamplers.Bicubic => KnownResamplers.Bicubic,
+            ImageSharpKnownResamplers.Box => KnownResamplers.Box,
+            ImageSharpKnownResamplers.CatmullRom => KnownResamplers.CatmullRom,
+            ImageSharpKnownResamplers.Hermite => KnownResamplers.Hermite,
+            ImageSharpKnownResamplers.Lanczos2 => KnownResamplers.Lanczos2,
+            ImageSharpKnownResamplers.Lanczos3 => KnownResamplers.Lanczos3,
+            ImageSharpKnownResamplers.Lanczos5 => KnownResamplers.Lanczos5,
+            ImageSharpKnownResamplers.Lanczos8 => KnownResamplers.Lanczos8,
+            ImageSharpKnownResamplers.MitchellNetravali => KnownResamplers.MitchellNetravali,
+            ImageSharpKnownResamplers.NearestNeighbor => KnownResamplers.NearestNeighbor,
+            ImageSharpKnownResamplers.Robidoux => KnownResamplers.Robidoux,
+            ImageSharpKnownResamplers.RobidouxSharp => KnownResamplers.RobidouxSharp,
+            ImageSharpKnownResamplers.Spline => KnownResamplers.Spline,
+            ImageSharpKnownResamplers.Triangle => KnownResamplers.Triangle,
+            ImageSharpKnownResamplers.Welch => KnownResamplers.Welch,
+            _ => throw new ArgumentOutOfRangeException(nameof(resampler), resampler, null)
+        };
+    }
+}
