d50727fc0912f8113d1dc909d37df0939dc92349
Josh Brunton <josh.brunton@proton.me>
2025-06-20 10:00:58 +0100

    test: Test dither, resampler conversions

---
+++ test/FlossApp.Application.Tests/Enums/ImageSharpKnownDitheringsTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FlossApp.Application.Enums;
+using FlossApp.Application.Extensions.FlossApp.Application.Enums;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Dithering;
+
+namespace FlossApp.Application.Tests.Enums;
+
+[TestClass]
+public class ImageSharpKnownDitheringsTests
+{
+    [TestMethod]
+    public void AllEnumsConvertToClasses()
+    {
+        foreach (var val in Enum.GetValues<ImageSharpKnownDitherings>())
+        {
+            ImageSharpKnownDitherings? nullable = val;
+            _ = nullable.AsKnownDithering();
+        }
+    }
+
+    [TestMethod]
+    public void AllClassesConvertToEnums()
+    {
+        IList<IDither?> dithersFromEnum = Enum.GetValues<ImageSharpKnownDitherings>()
+            .Select(x =>
+            {
+                ImageSharpKnownDitherings? nullable = x;
+                return nullable.AsKnownDithering();
+            }).ToList();
+
+        var dithersFromClass = typeof(KnownDitherings)
+            .GetProperties(BindingFlags.Public | BindingFlags.Static)
+            .Where(p => typeof(IDither).IsAssignableFrom(p.PropertyType))
+            .Select(p => (p.Name, Value: p.GetValue(null)));
+
+        foreach (var d in dithersFromClass)
+        {
+            if (d.Value is not IDither dither)
+            {
+                Assert.Fail($"Found member {d.Name}, which was not of type {nameof(IDither)}");
+                continue;
+            }
+
+            Assert.IsTrue(dithersFromEnum.Any(x => ReferenceEquals(x, dither)));
+        }
+    }
+}
---
+++ test/FlossApp.Application.Tests/Enums/ImageSharpKnownResamplersTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using FlossApp.Application.Enums;
+using FlossApp.Application.Extensions.FlossApp.Application.Enums;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
+
+namespace FlossApp.Application.Tests.Enums;
+
+[TestClass]
+public class ImageSharpKnownResamplersTests
+{
+    [TestMethod]
+    public void AllEnumsConvertToClasses()
+    {
+        foreach (var val in Enum.GetValues<ImageSharpKnownResamplers>())
+        {
+            _ = val.AsKnownResampler();
+        }
+    }
+
+    [TestMethod]
+    public void AllClassesConvertToEnums()
+    {
+        IList<IResampler> dithersFromEnum = Enum.GetValues<ImageSharpKnownResamplers>()
+            .Select(x => x.AsKnownResampler())
+            .ToList();
+
+        var dithersFromClass = typeof(KnownResamplers)
+            .GetProperties(BindingFlags.Public | BindingFlags.Static)
+            .Where(p => typeof(IResampler).IsAssignableFrom(p.PropertyType))
+            .Select(p => (p.Name, Value: p.GetValue(null)));
+
+        foreach (var d in dithersFromClass)
+        {
+            if (d.Value is not IResampler dither)
+            {
+                Assert.Fail($"Found member {d.Name}, which was not of type {nameof(IResampler)}");
+                continue;
+            }
+
+            Assert.IsTrue(dithersFromEnum.Any(x => ReferenceEquals(x, dither)));
+        }
+    }
+}
