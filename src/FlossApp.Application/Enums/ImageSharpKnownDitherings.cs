72c38c39aaeae32adff101d9fa21adcf9f0991cf
Josh Brunton <josh.brunton@proton.me>
2025-06-20 10:00:02 +0100

    Add enums for known dithers, resamplers

---
+++ src/FlossApp.Application/Enums/ImageSharpKnownDitherings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp.Processing.Processors.Dithering;
+
+namespace FlossApp.Application.Enums;
+
+public enum ImageSharpKnownDitherings
+{
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Bayer2x2" />
+    [Display(Name="Bayer 2x2")]
+    Bayer2x2,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Ordered3x3" />
+    [Display(Name="Ordered 3x3")]
+    Ordered3x3,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Bayer4x4" />
+    [Display(Name="Bayer 4x4")]
+    Bayer4x4,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Bayer8x8" />
+    [Display(Name="Bayer 8x8")]
+    Bayer8x8,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Bayer16x16" />
+    [Display(Name="Bayer 16x16")]
+    Bayer16x16,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Atkinson" />
+    Atkinson,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Burks" />
+    Burks,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.FloydSteinberg" />
+    [Display(Name="Floyd-Steinberg")]
+    FloydSteinberg,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.JarvisJudiceNinke" />
+    [Display(Name="Jarvis-Judice-Ninke")]
+    JarvisJudiceNinke,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Sierra2" />
+    [Display(Name="Sierra-2")]
+    Sierra2,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Sierra3" />
+    [Display(Name="Sierra-3")]
+    Sierra3,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.SierraLite" />
+    [Display(Name="Sierra-Lite")]
+    SierraLite,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.StevensonArce" />
+    [Display(Name="Stevenson-Acre")]
+    StevensonArce,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownDitherings.Stucki" />
+    Stucki,
+}
---
+++ src/FlossApp.Application/Enums/ImageSharpKnownResamplers.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlossApp.Application.Enums;
+
+public enum ImageSharpKnownResamplers
+{
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Bicubic" />
+    Bicubic,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Box" />
+    Box,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.CatmullRom" />
+    [Display(Name="Catmull-Rom")]
+    CatmullRom,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Hermite" />
+    Hermite,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Lanczos2" />
+    [Display(Name="Lanczos 2px")]
+    Lanczos2,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Lanczos3" />
+    [Display(Name="Lanczos 3px")]
+    Lanczos3,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Lanczos5" />
+    [Display(Name="Lanczos 5px")]
+    Lanczos5,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Lanczos8" />
+    [Display(Name="Lanczos 8px")]
+    Lanczos8,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.MitchellNetravali" />
+    [Display(Name = "Mitchell-Netravali")]
+    MitchellNetravali,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.NearestNeighbor" />
+    [Display(Name="Nearest neighbor")]
+    NearestNeighbor,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Robidoux" />
+    Robidoux,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.RobidouxSharp" />
+    [Display(Name="Robidoux Sharp")]
+    RobidouxSharp,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Spline" />
+    Spline,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Triangle" />
+    Triangle,
+
+    /// <inheritdoc cref="SixLabors.ImageSharp.Processing.KnownResamplers.Welch" />
+    Welch
+}
