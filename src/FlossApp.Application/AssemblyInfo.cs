2b0f5e2c0ebd02d6932784e7c50e7357850a9d95
Josh Brunton <josh.brunton@proton.me>
2025-07-01 14:27:48 +0100

    fix build errors in tests

--- src/FlossApp.Application/AssemblyInfo.cs
+++ src/FlossApp.Application/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("FlossApp.Application.Tests")]
+[assembly: InternalsVisibleTo("FlossApp.Application.Benchmarks")]
+[assembly: InternalsVisibleTo("FlossApp.Application.Mock")]
