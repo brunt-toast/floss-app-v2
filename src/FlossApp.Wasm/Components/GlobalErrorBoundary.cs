51d65c0bbd261ac7101f10bb26b340aff8f9e0e8
Josh Brunton <josh.brunton@proton.me>
2025-07-02 13:43:34 +0100

    better error display

---
+++ src/FlossApp.Wasm/Components/GlobalErrorBoundary.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace FlossApp.Wasm.Components;
+
+public class GlobalErrorBoundary : ErrorBoundary
+{
+    [Inject] private ILogger<GlobalErrorBoundary> Logger { get; set; } = null!;
+
+    protected override Task OnErrorAsync(Exception ex)
+    {
+        Logger.LogCritical(ex, "An unhandled exception reached the global error boundary. " +
+                               "Stack trace: {newline}{trace}", Environment.NewLine, Environment.StackTrace);
+        return Task.CompletedTask;
+    }
+}
---
+++ src/FlossApp.Wasm/Layout/MainLayout.razor.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using FlossApp.Application.Enums;
+using FlossApp.Application.Messages;
+using FlossApp.Wasm.Components;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace FlossApp.Wasm.Layout;
+
+public partial class MainLayout
+{
+    private GlobalErrorBoundary? _errorBoundary;
+
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        WeakReferenceMessenger.Default.Register<SnackbarMessage>(this, OnSnackbarMessage);
+    }
+
+    private void AttemptRecovery()
+    {
+        _errorBoundary?.Recover();
+    }
+
+    private void CopyErrorAsJson()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnSnackbarMessage(object recipient, SnackbarMessage message)
+    {
+        Severity severity = message.Severity switch
+        {
+            SnackbarSeverity.Normal => Severity.Normal,
+            SnackbarSeverity.Info => Severity.Info,
+            SnackbarSeverity.Success => Severity.Success,
+            SnackbarSeverity.Warn => Severity.Warning,
+            SnackbarSeverity.Error => Severity.Error,
+            null => Severity.Normal,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        Snackbar.Add(message.Message, severity, key: message.Key);
+    }
+}
