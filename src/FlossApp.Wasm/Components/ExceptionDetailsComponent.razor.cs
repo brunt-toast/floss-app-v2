fa981434779de393cdbe4d2ac8212bad6682d043
Josh Brunton <josh.brunton@proton.me>
2025-07-03 09:53:54 +0100

    Better exception details

---
+++ src/FlossApp.Wasm/Components/ExceptionDetailsComponent.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FlossApp.Wasm.Components;
+
+public partial class ExceptionDetailsComponent
+{
+    [Parameter]
+    public Exception Ex
+    {
+        get;
+        set
+        {
+            if (EqualityComparer<Exception>.Default.Equals(field, value))
+            {
+                return;
+            }
+
+            field = value;
+            ExChanged.InvokeAsync(value);
+        }
+    }
+
+    [Parameter] public EventCallback<Exception> ExChanged { get; set; }
+}
