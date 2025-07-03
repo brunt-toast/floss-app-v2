7b625fe43d959a2d046b0575c5443204e170b27d
Josh Brunton <josh.brunton@proton.me>
2025-07-03 15:48:39 +0100

    theming

---
+++ src/FlossApp.Wasm/Themes/CustomTheme1.cs
@@ -0,0 +1,14 @@
+﻿using MudBlazor;
+
+namespace FlossApp.Wasm.Themes;
+
+public class CustomTheme1 : MudTheme
+{
+    private static CustomTheme1? s_instance;
+    public static CustomTheme1 Instance => s_instance ??= new();
+
+    private CustomTheme1()
+    {
+        PaletteLight = new PaletteLight { Primary = Colors.LightGreen.Default };
+    }
+}
