08f605479c9d244fc669e8def58afa0ccbd8d955
Josh Brunton <josh.brunton@proton.me>
2025-07-11 11:03:05 +0100

    Add service resolution tests

---
+++ test/FlossApp.Application.Tests/Services/ServiceHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using FlossApp.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlossApp.Application.Tests.Services;
+
+[TestClass]
+public class ServiceHelperTests
+{
+    [TestMethod]
+    public void DeclaredServicesCanBeResolved()
+    {
+        var serviceCollection = ServiceHelper.GetInternalServiceDescriptors();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        foreach (ServiceDescriptor descriptor in serviceCollection)
+        {
+            _ = serviceProvider.GetRequiredService(descriptor.ServiceType);
+        }
+    }
+}
