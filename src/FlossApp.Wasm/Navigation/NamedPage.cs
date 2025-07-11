214d871bd1fa3e3b66fcdf16f995c92c0b2849a4
Josh Brunton <josh.brunton@proton.me>
2025-07-11 15:35:18 +0100

    Centralise named pages

---
+++ src/FlossApp.Wasm/Navigation/NamedPage.cs
@@ -0,0 +1,15 @@
+﻿namespace FlossApp.Wasm.Navigation;
+
+internal class NamedPage
+{
+    public string Icon { get; }
+    public string Name { get; }
+    public string Href { get; }
+
+    public NamedPage(string icon, string name, string href)
+    {
+        Icon = icon;
+        Name = name;
+        Href = href;
+    }
+}
---
+++ src/FlossApp.Wasm/Navigation/NamedPages.cs
@@ -0,0 +1,14 @@
+﻿using MudBlazor;
+
+namespace FlossApp.Wasm.Navigation;
+
+internal static class NamedPages
+{
+    public static IReadOnlyList<NamedPage> AllNamedPages { get; } =
+    [
+        new(Icons.Material.Filled.Compare, "Color Convert", "/FindSimilarColorsPage"),
+        new(Icons.Material.Filled.FilterHdr, "Image Filtering", "/ImageFilter"),
+        new(Icons.Material.Filled.LinearScale, "Upscaler", "/ImageUpscaler"),
+        new(Icons.Material.Filled.FormatSize, "Hoop sizer", "/HoopSizer"),
+    ];
+}
