120daff66c68399b2beb581f7086f0a04e73fb93
Josh Brunton <josh.brunton@proton.me>
2025-07-16 11:18:55 +0100

    Better color schema descriptors

--- src/FlossApp.Application/Enums/ColorSchema.cs
+++ src/FlossApp.Application/Enums/ColorSchema.cs
@@ -9,31 +9,31 @@ namespace FlossApp.Application.Enums;
 
 public enum ColorSchema
 {
-    [Display(Name= "RGB", Description="Red/Green/Blue")]
+    [Display(Name = "RGB", Description = "Red/Green/Blue. Widely used among computer scientists.")]
     Rgb,
 
-    [Display(Name = "Hex", Description="Hexadecimal RGB")]
+    [Display(Name = "Hex", Description = "Hexadecimal RGB. Alternative form of expression for RGB.")]
     RgbHex,
 
-    [Display(Name = "HSL", Description="Hue/Saturation/Lightness")]
+    [Display(Name = "HSL", Description = "Hue/Saturation/Lightness. Popular among design tools.")]
     Hsl,
 
-    [Display(Name= "DMC", Description="Dollfus-Mieg et Compagnie")]
+    [Display(Name = "DMC", Description = "Dollfus-Mieg et Compagnie. Alsatian textile company.")]
     Dmc,
 
-    [Display(Name = "HTML", Description="HyperText Markup Language")]
+    [Display(Name = "HTML", Description = "HyperText Markup Language. Used by web developers.")]
     Html,
 
-    [Display(Name = "COPIC", Description="\u30b3\u30d4\u30c3\u30af")]
+    [Display(Name = "COPIC", Description = "\u30b3\u30d4\u30c3\u30af. Japanese marker brand.")]
     Copic,
 
-    [Display(Name = "Anchor")]
+    [Display(Name = "Anchor", Description = "Sewing thread schema.")]
     Anchor,
 
-    [Display(Name = "Pantone")]
+    [Display(Name = "Pantone", Description = "Widely-used paint colors.")]
     Pantone,
 
-    [Display(Name="CMYK", Description="Cyan/Magenta/Yellow/Black")]
+    [Display(Name = "CMYK", Description = "Cyan/Magenta/Yellow/Black. Used by printers, photographers, and artists.")]
     Cmyk,
 
     [Display(Name = "CIE L*a*b*", Description = "Commission Internationale de l'\u00c9clairage: Lightness, Green-Red, Blue-Yellow")]
