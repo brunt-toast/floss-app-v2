8291a9ffec889490ca8380a03f8ee5994394d0e8
Josh Brunton <josh.brunton@proton.me>
2025-07-16 10:11:41 +0100

    Tests for algorithm resolution

---
+++ test/FlossApp.Application.Tests/Utils/ColorComparisonFuncsTests.cs
@@ -0,0 +1,17 @@
+﻿using FlossApp.Application.Enums;
+using FlossApp.Application.Utils;
+
+namespace FlossApp.Application.Tests.Utils;
+
+[TestClass]
+public class ColorComparisonFuncsTests
+{
+    [TestMethod]
+    public void AllColorComparisonAlgorithmsImplemented()
+    {
+        foreach (var value in Enum.GetValues<ColorComparisonAlgorithms>())
+        {
+            _ = ColorComparisonFuncs.GetComparisonAlgorithm(value);
+        }
+    }
+}
