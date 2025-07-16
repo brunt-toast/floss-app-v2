900d251bb7a27ca5d9ced3493cd22adda55c0de9
Josh Brunton <josh.brunton@proton.me>
2025-07-16 10:24:36 +0100

    Modify enum picker for descriptions

--- src/FlossApp.Wasm/Extensions/System/EnumExtensions.cs
+++ src/FlossApp.Wasm/Extensions/System/EnumExtensions.cs
@@ -7,11 +7,20 @@ public static class EnumExtensions
 {
     public static string ToDisplayName(this Enum source)
     {
+        return source.GetDisplayAttribute()?.GetName() ?? source.ToString();
+    }
+
+    public static string GetDescription(this Enum source)
+    {
+        return source.GetDisplayAttribute()?.GetDescription() ?? string.Empty;
+    }
+
+    private static DisplayAttribute? GetDisplayAttribute(this Enum source)
+    {
         string sourceString = source.ToString();
         return source.GetType()
             .GetMember(sourceString)
             .FirstOrDefault()?
-            .GetCustomAttribute<DisplayAttribute>()?
-            .GetName() ?? sourceString;
+            .GetCustomAttribute<DisplayAttribute>();
     }
 }
