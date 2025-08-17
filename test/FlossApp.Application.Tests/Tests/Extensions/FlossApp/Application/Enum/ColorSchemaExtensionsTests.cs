a247da1e4c04dea9e5d79bd0267ae848ea1a7f3e
Josh Brunton <josh.brunton@proton.me>
2025-08-17 12:10:58 +0100

    Add test for exhaustive schema rgb superset method

---
+++ test/FlossApp.Application.Tests/Tests/Extensions/FlossApp/Application/Enum/ColorSchemaExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using FlossApp.Application.Enums;
+using FlossApp.Application.Extensions.FlossApp.Application.Enums;
+using FlossApp.Application.Tests.Generators.Generic;
+
+namespace FlossApp.Application.Tests.Tests.Extensions.FlossApp.Application.Enum;
+
+[TestClass]
+public class ColorSchemaExtensionsTests
+{
+    [TestMethod]
+    [DynamicData(nameof(EnumGenerator<ColorSchema>.Generate), typeof(EnumGenerator<ColorSchema>), DynamicDataSourceType.Method)]
+    public void IsRgbSuperset_Handles_Exhaustive_Input(ColorSchema schema)
+    {
+        // throws exception if out of range
+        _ = schema.IsRgbSuperset();
+    }
+}
