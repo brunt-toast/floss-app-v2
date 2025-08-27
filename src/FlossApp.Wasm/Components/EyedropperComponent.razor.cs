a62808eb42a0e439d99e6732d7d0aecb4815f85d
Josh Brunton <josh.brunton@proton.me>
2025-08-27 10:49:10 +0100

    feat: UI for eyedropper

---
+++ src/FlossApp.Wasm/Components/EyedropperComponent.razor.cs
@@ -0,0 +1,82 @@
+﻿using FlossApp.Application.Enums;
+using Microsoft.AspNetCore.Components;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace FlossApp.Wasm.Components;
+
+public partial class EyedropperComponent
+{
+    [Parameter]
+    public Image<Rgba32> Image
+    {
+        get;
+        set
+        {
+            field = value;
+            _grid = CalculateGrid(value);
+        }
+    } = new(1, 1);
+
+    [Parameter]
+    public ColorSchema Schema
+    {
+        get => ViewModel.TargetSchema;
+        set
+        {
+            if (EqualityComparer<ColorSchema>.Default.Equals(ViewModel.TargetSchema, value))
+            {
+                return;
+            }
+
+            ViewModel.TargetSchema = value;
+            SchemaChanged.InvokeAsync(value);
+        }
+    }
+
+    [Parameter] public EventCallback<ColorSchema> SchemaChanged { get; set; }
+
+    [Parameter]
+    public ColorComparisonAlgorithms ComparisonAlgorithm
+    {
+        get => ViewModel.ComparisonAlgorithm;
+        set
+        {
+            if (EqualityComparer<ColorComparisonAlgorithms>.Default.Equals(ViewModel.ComparisonAlgorithm, value))
+            {
+                return;
+            }
+
+            ViewModel.ComparisonAlgorithm = value;
+            ComparisonAlgorithmChanged.InvokeAsync(value);
+        }
+    }
+
+    [Parameter] public EventCallback<ColorComparisonAlgorithms> ComparisonAlgorithmChanged { get; set; }
+    private List<List<System.Drawing.Color>> _grid = [];
+
+    private List<List<System.Drawing.Color>> CalculateGrid(Image<Rgba32> image)
+    {
+        Logger.LogInformation("Start");
+        List<List<System.Drawing.Color>> ret = [];
+
+        image.ProcessPixelRows(accessor =>
+        {
+            for (int y = 0; y < accessor.Height; y++)
+            {
+                var thisRow = new List<System.Drawing.Color>();
+                ret.Add(thisRow);
+
+                var rowSpan = accessor.GetRowSpan(y);
+                for (int x = 0; x < rowSpan.Length; x++)
+                {
+                    Logger.LogInformation($"{x}x{y}");
+                    var color = System.Drawing.Color.FromArgb(rowSpan[x].A, rowSpan[x].R, rowSpan[x].G, rowSpan[x].B);
+                    thisRow.Add(color);
+                }
+            }
+        });
+
+        return ret;
+    }
+}
---
+++ src/FlossApp.Wasm/Pages/EyedropperPage.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace FlossApp.Wasm.Pages;
+
+public partial class EyedropperPage
+{
+    private async Task UploadFiles(IBrowserFile? file)
+    {
+        if (file is null)
+        {
+            return;
+        }
+
+        try
+        {
+            await using var stream = file.OpenReadStream(maxAllowedSize: 1024 * 1024 * 1024);
+            await PageViewModel.LoadFileStreamAsync(stream);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
